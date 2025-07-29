98fe43c0bf6b1ab18db2ae3196439ab367a80383
UselessJoy <mrqwer2014@mail.ru>
2025-07-29 14:33:28 +0300

    maybe with redos

---
+++ tools/libs/pkglist-redos.sh
@@ -0,0 +1,7 @@
+### Crowsnest Dependencies
+
+set -Ee
+# crudini
+PKGLIST="git util-linux findutils v4l-utils curl"
+### Ustreamer Dependencies
+PKGLIST="${PKGLIST} gcc gcc-c++ make libevent-devel libjpeg-turbo-devel libbsd-devel pkgconf"
